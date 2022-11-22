969bba37dc44e0c83316019369ac57f6071fafbd
Michael Min <michaelmin2020@gmail.com>
2022-11-21 21:28:02 -0600

    created app component

--- react project/jsx/src/index.js
+++ react project/jsx/src/index.js
@@ -1,20 +1,12 @@
 // 1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
 // 2) Get a reference to the div with ID root
 const el = document.getElementById('root');
 // 3) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
-// 4) Create a component
-function App() {
-    // const inputType = 'number';
-    // const minValue = 5;
-    //return <input style={{ border: '3px solid red'}} type="number" min={5} />;
-    //return <textarea autoFocus={true} />;
-    //return <input max={5} />;
-    //return <input spellCheck={false} />;
-    return <input style={{ border: '3px solid red'}} />;
-}
-// 5) Show the component on the screen
+
+// 4) Show the component on the screen
 root.render(<App />);
